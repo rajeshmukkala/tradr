5db58557fede60f31d08c83f1103c2a2320186a2
rajeshmukkala <rajesh.mukkala@gmail.com>
2014-11-07 00:44:17 +0530

    Base

---
+++ config/tradr.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+
+	'dfeed' => array(
+		'http://www.moneycontrol.com/rss/MCtopnews.xml',
+		'http://businesstoday.intoday.in/rss/rssstory.jsp?sid=111' 
+	),
+	
+	'proxy' => 'http://tradr.app:8000/Proxies/?feed=',
+	'marketstats' => 'http://tradr.app:8000/feed/fii',
+	'NBfeed' => 'http://indices.moneycontrol.co.in/sensex_nifty/indtick.json?jsoncallback=indicereq',
+	'NSEfeed' => 'http://www.nseindia.com/homepage/Indices1.json',
+	'Fiiurl' => 'http://www.cdslindia.com/publications/FIIDailyData.aspx',
+	'fiiEQ' => 'http://www.nseindia.com/products/dynaContent/equities/equities/htms/fiiEQ.htm',
+	'DiiEQ' => 'http://www.nseindia.com/products/dynaContent/equities/equities/htms/DiiEQ.htm',
+	'SgxniftyURL' => 'http://sgx.com/JsonRead/JsonData?qryId=NTP.Futures&timeout=60',
+	'MarketStatus' => 'http://mtrade.sharekhan.com/marketStatus.php?exchange=NSE|BSE',
+	'NseOptionChain' => 'http://www.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?symbol=NIFTY&date=27NOV2014',
+	'STOCK_OPT_RANGE' => 100
+);
